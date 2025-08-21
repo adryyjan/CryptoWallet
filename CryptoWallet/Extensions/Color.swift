209f3f30aea29889d2e372be9daf5d3da7bc4376
Adrian Mazek <a6r14n@gmail.com>
2025-08-21 18:54:17 +0200

    set up

---
+++ CryptoWallet/Extensions/Color.swift
@@ -0,0 +1,20 @@
+//
+//  Color.swift
+//  CryptoWallet
+//
+//  Created by Adrian Mazek on 21/08/2025.
+//
+
+import SwiftUI
+
+extension Color {
+    static let theme = ColorTheme()
+}
+
+struct ColorTheme {
+     let accent = Color("AccentColor")
+     let background = Color("BackgroundColor")
+     let green = Color("GreenColor")
+     let red = Color("RedColor")
+     let secoundaryText = Color("SecondaryTextColor")
+}
